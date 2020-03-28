b59891d10b115fb593d40c3ef5ee979bc5177d2f
Scary MrGrey <imbamrgrey@gmail.com>
2020-03-28 21:38:53 +0200

    first impl

--- src/main/scala/NWC.scala
+++ src/main/scala/NWC.scala
@@ -15,6 +15,8 @@
  * limitations under the License.
  */
 
+import java.sql.Timestamp
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions.{window, _}
 import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType, TimestampType}
@@ -26,6 +28,7 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark._
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream.DStream
 
@@ -58,9 +61,9 @@ object NetworkWordCount {
     import org.apache.spark.sql.types._
     import spark.implicits._
     val postSchema: StructType = new StructType()
-      .add("commentCount", IntegerType, false)
+      .add("commentCount", IntegerType, true)
       .add("lastActivityDate", TimestampType, true)
-      .add("ownerUserId", LongType, true)
+      .add("ownerUserId", StringType, true)
       .add("body", StringType, true)
       .add("score", IntegerType, true)
       .add("creationDate", TimestampType, true)
@@ -68,25 +71,67 @@ object NetworkWordCount {
       .add("title", StringType, true)
       .add("tags", StringType, true)
       .add("answerCount", StringType, true)
-      .add("acceptedAnswerId", LongType, true)
-      .add("postTypeId", LongType, true)
+      .add("acceptedAnswerId", StringType, true)
+      .add("postTypeId", StringType, true)
       .add("id", LongType, false)
 
 
     val itPostsSplit = spark
       .read
       .option("delimiter", "~")
-      .option("header", "true")
       .schema(postSchema)
       .csv("italianPosts.csv")
 
+    val posts =
+      itPostsSplit
+        //.filter('postTypeId === 1)
+        //.select(avg("score"))
+        .withColumn("acceptedAnswerId", $"acceptedAnswerId".cast(LongType))
+        .withColumn("ownerUserId", $"ownerUserId".cast(LongType))
+        .withColumn("postTypeId", $"postTypeId".cast(LongType))
+        .withColumn("answerCount", $"answerCount".cast(LongType))
+        .withColumn("maxDateDiff", datediff('lastActivityDate, 'creationDate))
 
-    itPostsSplit.printSchema()
 
-    itPostsSplit
-      .filter('commentCount.isNull)
-      .show(10)
+    val itVotesRaw = spark.sparkContext.textFile("italianVotes.csv").
+      map(x => x.split("~"))
+    val itVotesRows = itVotesRaw.map(row => Row(row(0).toLong, row(1).toLong,
+      row(2).toInt, Timestamp.valueOf(row(3))))
+    val votesSchema = StructType(Seq(
+      StructField("id", LongType, false),
+      StructField("postId", LongType, false),
+      StructField("voteTypeId", IntegerType, false),
+      StructField("creationDate", TimestampType, false)))
+    val votesDf = spark.createDataFrame(itVotesRows, votesSchema)
 
+    posts
+      .join(votesDf, posts("id") === votesDf("postId"), "outer")
+    //  .show()
+
+    val playerMakeSchema = new StructType()
+      .add("id", IntegerType, false)
+      .add("Name", StringType, false)
+
+    val playerModelSchema = new StructType()
+      .add("id", IntegerType, false)
+      .add("Name", StringType, false)
+      .add("makeId", IntegerType, true)
+
+    val playerMakeDF = spark.read.schema(playerMakeSchema).csv("PlayerMake.csv")
+    val playerModelDF = spark.read.schema(playerModelSchema).csv("PlayerModel.csv")
+
+    //playerMakeDF.show()
+    //playerModelDF.show()
+
+//    playerModelDF
+//      .join(playerMakeDF, playerMakeDF("id") === 'makeId,"leftouter")
+//      .show()
+//    playerModelDF
+//      .join(playerMakeDF, playerMakeDF("id") === 'makeId,"left")
+//      .show()
+    playerModelDF
+      .join(playerMakeDF, playerMakeDF("id") === 'makeId,"right ")
+      .show()
     Console.readBoolean()
 
   }
